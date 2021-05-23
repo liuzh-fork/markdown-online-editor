368cf4c67ccd431ec26d14af4e4944af94cc4728
guozilan <912305749@qq.com>
2021-05-23 18:53:27 +0800

    update

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -10,7 +10,7 @@ yarn run build
 cd ./dist
 
 # 如果是发布到自定义域名
-echo 'markdown.lovejade.cn' > CNAME
+echo 'markdown.mybatis.io' > CNAME
 
 git init
 git add -A
@@ -19,5 +19,5 @@ git commit -m '🎉 update release'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-git push -f git@github.com:nicejade/markdown-online-editor.git master:gh-pages
+git push -f git@github.com:liuzh-fork/markdown-online-editor.git master:gh-pages
 cd -
